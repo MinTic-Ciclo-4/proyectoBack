520c44000fa59a41bf8fdb46f1d3d348e770cca9
Johan Forero <johanfore67@gmail.com>
2021-11-11 14:54:11 -0500

    Creacion de modelo y esquema de la coleccion Users

--- index.ts
+++ index.ts
@@ -1,7 +1,22 @@
 import conectarBD from "./db/db";
+import { UserModel } from "./models/user";
 
 const main = async () => {
   await conectarBD();
+
+  await UserModel.create({
+    correo: "johanfore67@gmail.com",
+    identificacion: "10047851",
+    nombre: "Johan",
+    apellido: "Forero"
+  })
+  .then(u => {
+    console.log("Usuario Creado", u);
+  })
+  .catch((e) => {
+    console.error("Error al crear el usuario", e);
+
+  })
 }
 
 main();
