e22b6c5196e91999f21d7ae0416384936d46889b
Johan Forero <johanfore67@gmail.com>
2021-11-11 15:17:58 -0500

    Creacion de Enum_Rol E Interfaz de Usuario

--- index.ts
+++ index.ts
@@ -1,22 +1,34 @@
 import conectarBD from "./db/db";
+import { Enum_Rol } from "./models/enums";
 import { UserModel } from "./models/user";
 
 const main = async () => {
   await conectarBD();
 
   await UserModel.create({
-    correo: "johanfore67@gmail.com",
-    identificacion: "10047851",
+    correo: "johanfore69@gmail.com",
+    identificacion: "1007445874",
     nombre: "Johan",
-    apellido: "Forero"
+    apellido: "Orozco",
+    rol: Enum_Rol.administrador,
   })
   .then(u => {
     console.log("Usuario Creado", u);
   })
   .catch((e) => {
     console.error("Error al crear el usuario", e);
+  });
 
-  })
+  //Obtener Usuarios
+
+  await UserModel.find()
+    .then((u) => {
+      console.log("usuarios", u);
+    })
+    .catch((e) => {
+      console.error("Error obteniendo los usuarios", e);
+    })
+
 }
 
 main();
--- models/user.ts
+++ models/user.ts
@@ -1,6 +1,14 @@
 import { Schema, model } from "mongoose";
+import { Enum_Rol } from "./enums";
+interface User {
+  correo: string;
+  identificacion: string;
+  nombre: string;
+  apellido: string;
+  rol: Enum_Rol;
+}
 
-const userSchema = new Schema({
+const userSchema = new Schema<User>({
   correo:{
     type: String,
     required: true,
@@ -17,6 +25,11 @@ const userSchema = new Schema({
   apellido: {
     type: String,
     required: true,
+  },
+  rol: {
+    type: String,
+    required: true,
+    enum: Enum_Rol,
   }
 });
 
