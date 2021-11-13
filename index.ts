36d47740e8b8aa70ed771877e136a13fd617c105
Johan Forero <johanfore67@gmail.com>
2021-11-12 22:39:05 -0500

    Creacion de esquema y modelo de Objetivos y relacion fuerte entre objetivos y proyecto

--- index.ts
+++ index.ts
@@ -1,10 +1,17 @@
 import conectarBD from "./db/db";
-import { Enum_Rol, Enum_EstadoUsuario } from "./models/enums";
+import { Enum_Rol, Enum_EstadoUsuario, Enum_TipoObjetivo } from "./models/enums";
+import { ObjectiveModel } from "./models/objective";
 import { ProjectModel } from "./models/project";
 import { UserModel } from "./models/user";
 
 const main = async () => {
   await conectarBD();
+
+
+  // const object = await ObjectiveModel.create({
+  //   descripcion: "Este es el objetivo especifico",
+  //   tipo: Enum_TipoObjetivo.especifico
+  // })
 
   //Crear Proyecto
 
@@ -13,7 +20,8 @@ const main = async () => {
   //   presupuesto: 200000,
   //   fechaInicio: Date.now(),
   //   fechaFin: new Date("2022/11/10"),
-  //   lider: "618f272d30bdc63f491d7c2e"
+  //   lider: "618f272d30bdc63f491d7c2e",
+  //   objetivos: ["618f2e8ba3d12a5129f19a1b", "618f2ec57ece6f1a431e8acf"]
   // })
   //   .then(u => {
   //     console.log("Proyecto Creado", u);
@@ -24,9 +32,8 @@ const main = async () => {
 
   //Buscar proyecto con relacion fuerte
 
-  const proyecto: any = await ProjectModel.find({nombre: "Proyecto 1"}).populate('lider');
-  console.log("El proyecto es ", proyecto);
-
+  const proyecto: any = await ProjectModel.find({nombre: "Proyecto 1"}).populate('objetivos');
+  console.log("El proyecto es ", JSON.stringify(proyecto));
 
   //Crear Usuario
 
--- models/project.ts
+++ models/project.ts
@@ -1,5 +1,6 @@
 import { Schema, model } from "mongoose";
 import { Enum_EstadoProyecto, Enum_FaseProyecto } from "./enums";
+import { ObjectiveModel } from "./objective";
 import { UserModel } from "./user";
 
 interface Project {
@@ -10,6 +11,7 @@ interface Project {
   estado : Enum_EstadoProyecto;
   fase: Enum_FaseProyecto;
   lider: Schema.Types.ObjectId;
+  objetivos: [Schema.Types.ObjectId];
 }
 
 const projectSchema = new Schema<Project>({
@@ -44,7 +46,14 @@ const projectSchema = new Schema<Project>({
     type: Schema.Types.ObjectId,
     required: true,
     ref: UserModel
-  }
+  },
+  objetivos: [
+    {
+      type: Schema.Types.ObjectId,
+      ref: ObjectiveModel
+    }
+  ]
+
 })
 
 const ProjectModel = model("Project", projectSchema);
