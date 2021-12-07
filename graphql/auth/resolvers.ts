780e9ebd031133758689bac0258b657307268fe2
Johan Forero <johanfore67@gmail.com>
2021-12-07 10:33:06 -0500

    Contraseña encriptada

--- graphql/auth/resolvers.ts
+++ graphql/auth/resolvers.ts
@@ -1,15 +1,20 @@
 import { UserModel } from '../../models/users/user';
+import bcrypt from 'bcrypt';
 
 const resolversAuth = {
   Mutation :{
     registro: async (parent, args) => {
+
+      const salt = bcrypt.genSalt(10);
+      const hashedPassword = await bcrypt.hash(args.password, salt);
+
       const usuarioCreado = await UserModel.create({
         nombre: args.nombre,
         apellido: args.apellido,
         identificacion: args.identificacion,
         correo: args.correo,
         rol: args.rol,
-        password: args.password,
+        password: hashedPassword,
       });
       return "usuarioCreado";
     }
