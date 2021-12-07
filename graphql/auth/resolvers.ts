c3db76ba1c8acf34ea248c2ba8eb582cfa4ddb37
Johan Forero <johanfore67@gmail.com>
2021-12-07 09:19:05 -0500

    Arreglo en el resolver de usuarios

--- graphql/auth/resolvers.ts
+++ graphql/auth/resolvers.ts
@@ -3,7 +3,7 @@
 const resolversAuth = {
   Mutation :{
     registro: async (parent, args) => {
-      console.log("Crear Usuario");
+      console.log("Crear Usuario", args);
       return 'Usuario Creado';
     }
   }
